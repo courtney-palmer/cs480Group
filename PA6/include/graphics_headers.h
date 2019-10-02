2b2644033e5685032ec375631adfdddc44d4ff06
christine-vaug <christinev@nevada.unr.edu>
2019-10-02 14:32:28 -0700

    Update graphics_headers.h

--- PA6/include/graphics_headers.h
+++ PA6/include/graphics_headers.h
@@ -25,14 +25,17 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/rotate_vector.hpp>
 
+#include <Magick++.h>
+
 #define INVALID_UNIFORM_LOCATION 0x7fffffff
 
 struct Vertex
 {
   glm::vec3 vertex;
   glm::vec3 color;
+  glm::vec2 texture;
 
-  Vertex(glm::vec3 v, glm::vec3 c): vertex(v), color(c) {}
+  Vertex(glm::vec3 v, glm::vec3 c, glm::vec2 t;): vertex(v), color(c), texture(t) {}
 };
 
 #endif /* GRAPHICS_HEADERS_H */
